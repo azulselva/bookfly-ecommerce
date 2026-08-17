0bfde91b226f21dcc641e785ce3603df850fea89
Sandra <mazsan@gmail.com>
2026-08-17 16:54:06 +0200

    footer hinzufügen

---
+++ src/includes/footer.php
@@ -0,0 +1,19 @@
+        <footer class="footer">
+        <hr class="footer-hours">
+        <h2>Bookfly-Büchershop-Öffnungszeiten</h2>
+
+        <dl>
+            <dt>Mo-Fr: </dt>
+            <dd>9:00-20:30 Uhr</dd>
+            <dt>sa: </dt>
+            <dd>9:00-20 Uhr</dd>
+            <dt>so: </dt>
+            <dd>13:00-17 Uhr</dd>
+        </dl>
+        <hr class="footer-divider">
+        <address>© 2026 Bookfly GmbH Eulenstraße 11 10115 Smaragdstadt</address>
+    </footer>  
+    
+</body>
+
+</html>
--- src/public/index.php
+++ src/public/index.php
@@ -65,24 +65,7 @@
         </ul>
     </main>  
     
-    
-    <footer class="footer">
-        <hr class="footer-hours">
-        <h2>Bookfly-Büchershop-Öffnungszeiten</h2>
+ <?php require_once '../includes/footer.php';?>    
 
-        <dl>
-            <dt>Mo-Fr: </dt>
-            <dd>9:00-20:30 Uhr</dd>
-            <dt>sa: </dt>
-            <dd>9:00-20 Uhr</dd>
-            <dt>so: </dt>
-            <dd>13:00-17 Uhr</dd>
-        </dl>
-        <hr class="footer-divider">
-        <address>© 2026 Bookfly GmbH Eulenstraße 11 10115 Smaragdstadt</address>
-    </footer>  
 
-</body>
-
-</html>
 
