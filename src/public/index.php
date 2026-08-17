2279ffdc403022bb1616b73ec1eb416a5c0c5a9f
Sandra <mazsan@gmail.com>
2026-08-17 16:47:23 +0200

    header hinzufugen

--- src/public/index.php
+++ src/public/index.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookfly Books online shop</title>
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="normalize.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
-</head>
-<body>
-    <header>
-        <img class="logo" src="img/Bookfly-logo-kurz-transparent.png">
-    </header>    
+<?php require_once '../includes/header.php';?> 
+
     <div class="banner-basic"> Dein Geist reist mit den Besten Büchern - Wir sind deine Brücke </div>
     <div class="banner-temp-container">
         <div class="banner-temp"> In  kurze eröffnen wir  unseren Online-Shop </div>
